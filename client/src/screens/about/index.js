301c7509c3266bb5fc6a780c99eafa91c82738ef
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2014-05-31 20:54:55 +0200

    Migrate About screen to React

--- client/src/screens/about/index.js
+++ client/src/screens/about/index.js
@@ -1,27 +1,13 @@
-
 var Qajax = require("qajax");
-var template = require("./screen.hbs");
-var hljs = require('highlight.js');
-var _ = require("lodash");
+var Article = require("./article");
 
 function show () {
-  var elt = document.createElement("div");
-  elt.innerHTML = template({});
-  var content = elt.querySelector(".content");
   return Qajax("/api/home")
     .then(Qajax.filterSuccess)
     .get("responseText")
     .then(function (html) {
-      content.innerHTML = html;
-      return content;
-    })
-    .then(function highlightAllCode (node) {
-      _.each(node.querySelectorAll("pre code"), function (code) {
-        code.innerHTML = hljs.highlight("javascript", code.innerHTML).value;
-        code.className += " hljs";
-      });
-    })
-    .thenResolve({ elt: elt });
+      return Article({ content: html });
+    });
 }
 
 function init () {
