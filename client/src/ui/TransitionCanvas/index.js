22917b8e545a2f96b4197f0ccb79997ed3beee68
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2014-06-09 14:39:28 +0200

    TransitionCanvas

---
+++ client/src/ui/TransitionCanvas/index.js
@@ -0,0 +1,89 @@
+/** @jsx React.DOM */
+var React = require("react");
+var _ = require("lodash");
+var GlslTransition = require("glsl-transition");
+
+var validSampler2D = React.PropTypes.oneOfType([
+  React.PropTypes.instanceOf(window.HTMLImageElement),
+  React.PropTypes.instanceOf(window.HTMLCanvasElement)
+]);
+
+var TransitionCanvas = React.createClass({ // FIXME need to implement more cache and lazy-ness
+  propTypes: {
+    glsl: React.PropTypes.string.isRequired,
+    from: validSampler2D,
+    to: validSampler2D,
+    uniforms: React.PropTypes.object.isRequired,
+    width: React.PropTypes.number.isRequired, // FIXME handle width/height changes
+    height: React.PropTypes.number.isRequired
+  },
+  render: function () {
+    var dpr = window.devicePixelRatio || 1;
+    var width = this.props.width;
+    var height = this.props.height;
+    return (
+      <canvas ref="render" width={width*dpr} height={height*dpr} style={{width: width+"px", height: height+"px"}}></canvas>
+    );
+  },
+  componentDidMount: function () {
+    this.running = 0;
+    this.Transition = GlslTransition(this.getDOMNode());
+    this.componentDidUpdate();
+  },
+  componentWillUnmount: function () {
+    if (this.transition) this.transition.destroy();
+    this.transition = null;
+    this.Transition = null;
+  },
+  componentDidUpdate: function () {
+    if (this.props.glsl !== this.lastGlsl) {
+      this.syncGlsl();
+    }
+    else if (
+      this.props.to !== this.lastTo ||
+      this.props.from !== this.lastFrom ||
+      !_.isEqual(this.props.uniforms, this.lastUniforms) ||
+      this.props.progress !== this.lastProgress
+    ) {
+      this.syncUniforms();
+    }
+    this.lastTo = this.props.to;
+    this.lastFrom = this.props.from;
+    this.lastUniforms = this.props.uniforms;
+    this.lastProgress = this.props.progress;
+    this.lastGlsl = this.props.glsl;
+  },
+  syncGlsl: function () {
+    var glsl = this.props.glsl;
+    if (this.transition) {
+      this.Transition.abort();
+      this.transition.destroy();
+    }
+    this.transition = this.Transition(glsl);
+    this.syncUniforms();
+  },
+  syncUniforms: function () {
+    this.transition.reset();
+    _.each(this.getAllUniforms(), function (value, u) {
+      this.transition.setUniform(u, value);
+    }, this);
+    if (!this.running) {
+      this.transition.setUniform("progress", this.props.progress);
+      this.transition.draw();
+    }
+  },
+  getAllUniforms: function () {
+    return _.extend({ from: this.props.from, to: this.props.to }, this.props.uniforms);
+  },
+  animate: function (duration) {
+    this.transition.reset();
+    var p = this.transition(this.getAllUniforms(), duration);
+    this.running++;
+    p.fin(_.bind(function () {
+      this.running--;
+    }, this));
+    return p;
+  },
+});
+
+module.exports = TransitionCanvas;
--- client/src/ui/vignette/index.js
+++ client/src/ui/vignette/index.js
@@ -1,120 +1,14 @@
 /** @jsx React.DOM */
 var React = require("react");
 var _ = require("lodash");
-var Q = require("q");
 var GlslTransition = require("glsl-transition");
+var Q = require("q");
+var TransitionCanvas = require("../TransitionCanvas");
 var Link = require("../Link");
 
 function circular (n, l) {
   return n < l ? n : 0;
 }
-
-var validSampler2D = React.PropTypes.oneOfType([
-  React.PropTypes.instanceOf(window.HTMLImageElement),
-  React.PropTypes.instanceOf(window.HTMLCanvasElement)
-]);
-
-var Transition = React.createClass({ // FIXME need to implement more cache and lazy-ness
-  propTypes: {
-    glsl: React.PropTypes.string.isRequired,
-    from: validSampler2D,
-    to: validSampler2D,
-    uniforms: React.PropTypes.object.isRequired,
-    width: React.PropTypes.number.isRequired,
-    height: React.PropTypes.number.isRequired
-  },
-  render: function () {
-    var dpr = window.devicePixelRatio || 1;
-    var width = this.props.width;
-    var height = this.props.height;
-    return (
-      <canvas ref="render" width={width*dpr} height={height*dpr} style={{width: width+"px", height: height+"px"}}></canvas>
-    );
-  },
-  componentDidMount: function () {
-    this.running = 0;
-    this.Transition = GlslTransition(this.getDOMNode());
-    this.lastTo = this.props.to;
-    this.lastFrom = this.props.from;
-    this.onNewImages();
-    this.lastUniforms = this.props.uniforms;
-    this.onNewUniforms();
-    this.lastProgress = this.props.progress;
-    this.onNewProgress();
-    this.lastGlsl = this.props.glsl;
-    this.onNewGlsl();
-  },
-  componentWillUnmount: function () {
-    if (this.transition) this.transition.destroy();
-    this.transition = null;
-    this.Transition = null;
-  },
-  componentDidUpdate: function () {
-    if (this.props.to !== this.lastTo || this.props.from !== this.lastFrom) {
-      this.lastTo = this.props.to;
-      this.lastFrom = this.props.from;
-      this.onNewImages();
-    }
-    if (!_.isEqual(this.props.uniforms, this.lastUniforms)) {
-      this.lastUniforms = this.props.uniforms;
-      this.onNewUniforms();
-    }
-    if (this.props.glsl !== this.lastGlsl) {
-      this.lastGlsl = this.props.glsl;
-      this.onNewGlsl();
-    }
-    if (this.props.progress !== this.lastProgress) {
-      this.lastProgress = this.props.progress;
-      this.onNewProgress();
-    }
-  },
-  onNewGlsl: function () {
-    var glsl = this.props.glsl;
-    if (this.transition) {
-      this.Transition.abort();
-      this.transition.destroy();
-    }
-    this.transition = this.Transition(glsl);
-    this.syncUniforms();
-  },
-  onNewUniforms: function () {
-    if (this.transition) {
-      this.syncUniforms();
-    }
-  },
-  onNewImages: function () {
-    if (this.transition) {
-      this.syncUniforms();
-    }
-  },
-  onNewProgress: function () {
-    if (this.transition) {
-      this.syncUniforms();
-    }
-  },
-  syncUniforms: function () {
-    this.transition.reset();
-    _.each(this.getAllUniforms(), function (value, u) {
-      this.transition.setUniform(u, value);
-    }, this);
-    if (!this.running) {
-      this.transition.setUniform("progress", this.props.progress);
-      this.transition.draw();
-    }
-  },
-  getAllUniforms: function () {
-    return _.extend({ from: this.props.from, to: this.props.to }, this.props.uniforms);
-  },
-  animate: function (duration) {
-    this.transition.reset();
-    var p = this.transition(this.getAllUniforms(), duration);
-    this.running++;
-    p.fin(_.bind(function () {
-      this.running--;
-    }, this));
-    return p;
-  },
-});
 
 var Vignette = React.createClass({
 
@@ -148,7 +42,7 @@ var Vignette = React.createClass({
 
     return (
     <div className="vignette" style={{width: this.props.width+"px", height: this.props.height+"px"}}>
-      <Transition ref="transition" progress={this.state.progress} width={this.props.width} height={this.props.height} glsl={this.props.glsl} uniforms={this.props.uniforms} from={from} to={to} />
+      <TransitionCanvas ref="transition" progress={this.state.progress} width={this.props.width} height={this.props.height} glsl={this.props.glsl} uniforms={this.props.uniforms} from={from} to={to} />
       {this.props.children}
       <OverlayElement href={this.props.href} className="overlay" ref="overlay" onMouseMove={this.onMouseMove} onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave}>
         <span className="cursor" ref="cursor" style={{ left: (this.state.progress * 100)+"%" }}></span>
