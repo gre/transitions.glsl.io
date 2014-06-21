b353bf7d29daafd56f7892f81fb459acd71d211d
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2014-06-21 15:25:57 +0200

    Add autocompletion in GLSL editor :-)

--- client/src/screens/editor/GlslEditor/index.js
+++ client/src/screens/editor/GlslEditor/index.js
@@ -26,7 +26,11 @@ var GlslEditor = React.createClass({
     this._lastWidth = this.props.width;
     this._lastHeight = this.props.height;
     var node = this.getDOMNode();
+    ace.require("ace/ext/language_tools");
     var editor = ace.edit(node);
+    editor.setOptions({
+      enableBasicAutocompletion: true
+    });
     editor.commands.addCommand({
       name: 'save',
       bindKey: {win: 'Ctrl-S',  mac: 'Command-S'},
