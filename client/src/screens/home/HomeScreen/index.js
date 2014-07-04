01471662d28c87549e4901dfbc89334255056e55
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2014-07-04 22:57:06 +0200

    Improve HomeScreen + fix user-select

--- client/src/screens/home/HomeScreen/index.js
+++ client/src/screens/home/HomeScreen/index.js
@@ -2,6 +2,7 @@
 var React = require("react");
 var Vignette = require("../../../ui/Vignette");
 var GLSLio = require("../../../ui/Logo");
+var Link = require("../../../ui/Link");
 
 var HomeScreen = React.createClass({
 
@@ -30,6 +31,9 @@ var HomeScreen = React.createClass({
       <h2>
         WebGL Transitions for your images slideshow
       </h2>
+      <p>
+        This slideshow shows all transitions created by <GLSLio /> contributors!
+      </p>
       <Vignette
         images={this.props.images}
         autostart={true}
@@ -45,9 +49,71 @@ var HomeScreen = React.createClass({
           <strong>{transition.owner}</strong>
           </span>
       </Vignette>
-      <p>
-        This slideshow shows all transitions created by <strong><GLSLio /></strong> contributors!
-      </p>
+
+      <h3>GLSL Transitions are...</h3>
+
+      <dl>
+
+        <dt><i className="fa fa-tachometer"></i> Highly Performant</dt>
+        <dd>
+        Incredible Transition Effects running at 60 FPS in your browser.
+        </dd>
+
+        <dt><i className="fa fa-magic"></i> Incredible Effects</dt>
+        <dd>
+        GLSL is really <strong>the</strong> ultimate language to implement Transitions in.
+        There is really no limitation on effects you can perform with.
+        </dd>
+
+        <dt><i className="fa fa-cogs"></i> Entirely Customisable</dt>
+        <dd>
+          A transition exposes "uniform" that is helpful to customize the effect parameters.
+          Transition Duration and Transition Easing Function are also customisable.
+        </dd>
+
+        <dt><i className="fa fa-puzzle-piece"></i> Multi-environnment</dt>
+        <dd>
+          GLSL can be supported both on Browsers and on Native environnment.
+          We are working to make GLSL Transitions working in Video Editors.
+        </dd>
+
+        <dt><i className="fa fa-film"></i> Multi-usage</dt>
+        <dd>
+          GLSL Transitions focus on defining a transition between 2 sources.
+          Those can be images, videos or anything 2D!
+        </dd>
+
+        <dt><i className="fa fa-cloud-download"></i> Easy to use</dt>
+        <dd>
+          The <Link href="https://github.com/glslio/glsl-transition">glsl-transition</Link> library make GLSL Transitions very easy to use on your webpages.
+        </dd>
+
+      </dl>
+
+      <h3>
+        <GLSLio /> is...
+      </h3>
+
+      <dl>
+        <dt><i className="fa fa-users"></i> Community Driven</dt>
+        <dd>
+          GLSL Transitions are created by people, for people.
+        </dd>
+        <dt><i className="fa fa-code"></i> Free License Transitions</dt>
+        <dd>
+        All transitions are released under Free License.
+        By creating content on <GLSLio />, you accept to release transitions under MIT License.
+        </dd>
+        <dt><i className="fa fa-code"></i> Entirely Open Source</dt>
+        <dd>
+        The source code of <GLSLio /> itself is fully available <Link href="https://github.com/">on Github</Link>.
+        </dd>
+        <dt><i className="fa fa-github-alt"></i> Gist Hosted</dt>
+        <dd>
+          No data are kept on our server.
+          All transitions are stored in Gists and owned by the community.
+        </dd>
+      </dl>
 
     </div>;
   }
