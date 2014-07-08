c7254d88379c02bdad7a6cd826a77dffa7d68739
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2014-07-09 00:37:44 +0200

    wip

--- client/src/screens/home/HomeScreen/index.js
+++ client/src/screens/home/HomeScreen/index.js
@@ -5,6 +5,7 @@ var NumberInput = require("../../../ui/NumberInput");
 var BezierEditor = require("../../../ui/BezierEditor");
 var Slideshow = require("../../../ui/Slideshow");
 var GLSLio = require("../../../ui/Logo");
+var Button = require("../../../ui/Button");
 var Link = require("../../../ui/Link");
 var Fps = require("../../editor/Fps");
 
@@ -18,6 +19,7 @@ var HomeScreen = React.createClass({
 
   getInitialState: function () {
     return {
+      page: 0,
       fps: null,
       videoTransition: this.props.transitions[Math.floor(Math.random()*this.props.transitions.length)],
       easing: [0.25, 0.25, 0.75, 0.75],
@@ -40,88 +42,124 @@ var HomeScreen = React.createClass({
     this.setState({ easing: easing });
   },
 
-  render: function () {
-    return <div className="home-screen">
-      <h2>
-        WebGL Transitions for your images slideshow
-      </h2>
-      <p>
-        This slideshow shows all transitions created by <GLSLio /> contributors!
+  prev: function () {
+    this.setState({ page: this.state.page - 1 });
+  },
 
-        <Slideshow width={512} height={384} images={this.props.images} transitions={this.props.transitions} onSlideChange={this.onSlideChange} transitionEasing={BezierEasing.apply(null, this.state.easing)} transitionDuration={this.state.duration} />
-      </p>
+  next: function () {
+    this.setState({ page: this.state.page + 1 });
+  },
 
-      <h3>GLSL Transitions are...</h3>
+  slideshows: {
+    images: {
+      render: function () {
+        return <Slideshow width={512} height={384} images={this.props.images} transitions={this.props.transitions} onSlideChange={this.onSlideChange} transitionEasing={BezierEasing.apply(null, this.state.easing)} transitionDuration={this.state.duration} />;
+      }
+    },
+    videos: {
+      render: function () {
+        // FIXME should not use an iframe here.. + slideshow loop forever
+        return <iframe width={512} height={288} src={"/transition/"+this.state.videoTransition.id+"/embed?video=1"} frameBorder="0" seamless="seamless"></iframe>;
+      }
+    }
+  },
 
-      <dl>
-
-        <dt><i className="fa fa-tachometer"></i> Highly Performant</dt>
-        <dd>
-        Incredible Transition Effects running at 60 FPS in your browser.
-        </dd>
-        <dd>
-        The current slideshow transition is running at: <Fps fps={this.state.fps} />
-        </dd>
-      </dl>
-
-      <dl>
-        <dt><i className="fa fa-magic"></i> Incredible Effects</dt>
-        <dd>
+  pages: [
+    {
+      slideshow: "images",
+      icon: "tachometer",
+      title: "Highly Performant",
+      render: function () {
+        return <div>
+        <div>
+          Incredible Transition Effects running at 60 FPS in your browser.
+        </div>
+        <div>
+          The current slideshow transition is running at: <Fps fps={this.state.fps} />
+        </div>
+        </div>;
+      }
+    },
+    {
+      slideshow: "images",
+      icon: "magic",
+      title: "Incredible Effects",
+      render: function () {
+        return <div>
+        <div>
         GLSL is really <strong>the</strong> ultimate language to implement Transitions in.
         There is really no limitation on effects you can perform with.
-        </dd>
-        <dd>
+        </div>
+        <div>
         <Link href="/gallery">
           <img src="/assets/examples.png" style={{width: "500px"}} alt="" />
         </Link>
-        </dd>
-      </dl>
-
-      <dl>
-        <dt><i className="fa fa-film"></i> Video Ready!</dt>
-        <dd>
+        </div>
+        </div>;
+      }
+    },
+    {
+      slideshow: "videos",
+      icon: "film",
+      title: "Video Ready!",
+      render: function () {
+        return <div>
+        <div>
           GLSL Transitions focus on defining a transition between 2 sources.
-        </dd>
-        <dd>
+        </div>
+        <div>
           Those can be images, videos or anything 2D!
-          {/* TODO we want this to work without iframe */}
-<iframe width={512} height={288} src={"/transition/"+this.state.videoTransition.id+"/embed?video=1"} frameBorder="0" seamless="seamless"></iframe>
-
-        </dd>
-      </dl>
-
-      <dl>
-        <dt><i className="fa fa-puzzle-piece"></i> Multi-environnment</dt>
-        <dd>
+        </div>
+        </div>;
+      }
+    },
+    {
+      slideshow: "images",
+      icon: "puzzle-piece",
+      title: "Multi-environnment",
+      render: function () {
+        return <div>
+        <div>
           GLSL can be supported both on Browsers and on Native environnment.
           We are working to make GLSL Transitions working in Video Editors.
-        </dd>
-      </dl>
-
-      <dl>
-        <dt><i className="fa fa-cogs"></i> Entirely Customisable</dt>
-        <dd>
+        </div>
+        </div>;
+      }
+    },
+    {
+      slideshow: "images",
+      icon: "cogs",
+      title: "Entirely Customisable",
+      render: function () {
+        return <div>
+        <div>
           A transition exposes "uniform" that is helpful to customize the effect parameters.
           Transition Duration and Transition Easing Function are also customisable.
-        </dd>
-        <dd className="duration">
-        <strong>Duration:</strong>
-        <span className="value">{this.state.duration}ms</span>
-        <NumberInput onChange={this.onDurationChange} type="range" step={50} min={100} max={3000} value={this.state.duration} />
-        </dd>
-        <dd>
-        <strong>Easing:</strong>
-        <BezierEditor value={this.state.easing} onChange={this.setEasing} width={500} height={300} handleRadius={8} padding={[20, 100, 20, 100]} />
-        </dd>
-      </dl>
-
-      <dl>
-        <dt><i className="fa fa-cloud-download"></i> Easy to use</dt>
-        <dd>
+        </div>
+        <div className="duration">
+          <strong>Duration:</strong>
+          <span className="value">{this.state.duration}ms</span>
+          <NumberInput onChange={this.onDurationChange} type="range" step={50} min={100} max={3000} value={this.state.duration} />
+        </div>
+        <div>
+          <strong>Easing:</strong>
+          <BezierEditor value={this.state.easing} onChange={this.setEasing} width={500} height={300} handleRadius={8} padding={[20, 100, 20, 100]} />
+        </div>
+        </div>;
+      }
+    },
+    {
+      slideshow: "images",
+      icon: "cloud-download",
+      title: "Easy to use",
+      render: function () {
+        return <div>
           The <Link href="https://github.com/glslio/glsl-transition">glsl-transition</Link> library make GLSL Transitions very easy to use on your webpages.
-        </dd>
-
-      </dl>
+        </div>;
+      }
+    }
+  ],
+  /*
 
       <h3>
         <GLSLio /> is...
@@ -129,35 +167,71 @@ var HomeScreen = React.createClass({
 
       <dl>
         <dt><i className="fa fa-users"></i> Community Driven</dt>
-        <dd>
+        <div>
           GLSL Transitions are created by people, for people.
-        </dd>
+        </div>
       </dl>
 
       <dl>
         <dt><i className="fa fa-code"></i> Free License Transitions</dt>
-        <dd>
+        <div>
         All transitions are released under Free License.
         By creating content on <GLSLio />, you accept to release transitions under MIT License.
-        </dd>
+        </div>
       </dl>
 
       <dl>
         <dt><i className="fa fa-code"></i> Entirely Open Source</dt>
-        <dd>
+        <div>
         The source code of <GLSLio /> itself is fully available <Link href="https://github.com/">on Github</Link>.
-        </dd>
+        </div>
       </dl>
 
       <dl>
         <dt><i className="fa fa-github-alt"></i> Gist Hosted</dt>
-        <dd>
+        <div>
           No data are kept on our server.
           All transitions are stored in Gists and owned by the community.
-        </dd>
+        </div>
       </dl>
 
       <br style={{"clear":"both"}} />
+      */
+
+  render: function () {
+    var page = this.pages[this.state.page];
+    var slideshow = this.slideshows[page.slideshow].render.call(this);
+    var pageContent = page.render.call(this);
+
+    /*
+     * TODO very WIP
+     * - better display of pagination + display all icons to easily switch / preview all pages
+     * - more responsive
+     * - Better video integration: shouldn't change the ratio + auto run & loop
+     * - More pages:
+     *   - App tour (showing the main video)
+     */
+
+    return <div className="home-screen">
+      <h2>
+        WebGL Transitions for your images slideshow
+      </h2>
+      <div>
+        This slideshow shows all transitions created by <GLSLio /> contributors!
+
+        {slideshow}
+      </div>
+
+      <div className="paginator">
+        <Button f={this.prev}>Prev</Button> - 
+        <Button f={this.next}>Next</Button>
+      </div>
+
+      <div className="page">
+        <header><i className={"fa fa-"+page.icon}></i> {page.title}</header>
+        {pageContent}
+      </div>
+
     </div>;
   }
 
