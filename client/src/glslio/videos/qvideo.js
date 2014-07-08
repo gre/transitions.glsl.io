038f10bac8c7119ba32c20b023c9233838fe61da
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2014-07-08 19:08:18 +0200

    qvideo uses qajax actually helps for loading the asset in FF -_-

--- client/src/glslio/videos/qvideo.js
+++ client/src/glslio/videos/qvideo.js
@@ -1,4 +1,5 @@
 var Q = require("q");
+var Qajax = require("qajax");
 
 /**
  * options.event different level of load:
@@ -8,20 +9,28 @@ var Q = require("q");
  *  - "canplaythrough" (default)
  */
 function Qvideo (url, options) {
-  if (typeof options !== "object") options={};
-  if (!options.event) options.event = "canplaythrough";
-  var d = Q.defer();
-  var video = document.createElement('video');
-  video.src = url;
-  video.addEventListener(options.event, function () {
-    d.resolve(video);
-  }, true);
-  video.onerror = function (e) {
-    console.error(e&&e.stack||e);
-    d.reject(new Error("Video "+url+" failed to load: "+e));
-  };
-  video.load();
-  return d.promise;
+  return Qajax(url).then(function(){
+    if (typeof options !== "object") options={};
+    if (!options.event) options.event = "canplaythrough";
+    var d = Q.defer();
+    var video;
+    try {
+      video = new window.Video();
+    } catch (e) {
+      video = document.createElement('video');
+    }
+
+    video.src = url;
+    video.addEventListener(options.event, function () {
+      d.resolve(video);
+    }, true);
+    video.onerror = function (e) {
+      console.error(e&&e.stack||e);
+      d.reject(new Error("Video "+url+" failed to load: "+e));
+    };
+    video.load();
+    return d.promise;
+  });
 }
 
 module.exports = Qvideo;
