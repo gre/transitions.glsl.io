cfc40c6eae5bf7fd9eead8ad9783fcdaa2472719
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2014-07-09 23:35:47 +0200

    Update deploy

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
- 
+
 branch=`git rev-parse --abbrev-ref HEAD`
 # We assume that branch==domain if you are using this script (convention)
 
-REMOTE=play@$branch
-REMOTE_APP=/home/play/$branch/
+REMOTE=$branch
+REMOTE_APP=/root/
 
 echo "Will upload to... "$REMOTE":"$REMOTE_APP;
 sleep 2 || exit;
@@ -22,5 +22,4 @@ cd -;
 cd server;
 sbt stage || exit 3;
 rsync -va target/ $REMOTE:$REMOTE_APP/target
-ssh $REMOTE "cd $REMOTE_APP; ./stop.sh";
-ssh $REMOTE "cd $REMOTE_APP; ./start.sh";
+ssh $REMOTE "service playapp restart"
