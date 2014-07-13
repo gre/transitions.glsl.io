65d711f1008c0db1613b1b8822481e486bc83a45
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2014-07-13 16:37:06 +0200

    update publish-transitions scripts

--- scripts/publish-transitions/run.sh
+++ scripts/publish-transitions/run.sh
@@ -8,17 +8,29 @@ VERSION=${2-$NOW_VERSION}
 
 cd target || exit 1
 
-git pull origin master || exit 2
+echo reset repository...
+git fetch || exit 2
+git checkout master || exit 2
+git reset --hard origin/master || exit 2
 
+echo versionning...$VERSION
+npm version $VERSION || exit 6
+
+echo retrieve and compile transitions...
 rm -f transitions.glsl
 curl $SERVER/api/snapshots/transitions > transitions.json || exit 3
 
 node test.js || exit 4
 
-echo versionning...$VERSION
-npm version $VERSION || exit 5
+npm install || exit 5
+npm run build || exit 5
 
-npm publish || exit 6
+echo add files...
+git add transitions.json standalone.js || exit 6
+git add package.json || exit 7
 
-git add transitions.json
-git commit -a -m"$VERSION" && git push origin master
+echo publish...
+npm publish || exit 8
+git commit -m"$VERSION" && git push origin master
+
+echo done.
