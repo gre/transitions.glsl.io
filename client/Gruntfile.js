ff4f5eebe962871ea1ccff846dafbacc25484fd7
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2014-07-14 21:39:54 +0200

    Add loader

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -64,7 +64,7 @@ module.exports = function (grunt) {
       },
       js: {
         files: ['src/**/*.js'],
-        tasks: ['jshint', 'browserify', 'uglify'],
+        tasks: ['jshint', 'browserify'],
       },
       css: {
         files: ['src/**/*.styl'],
